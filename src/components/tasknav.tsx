607b1a37441f5fbf338f5e422aac5faac3a0ecef
ConquerorCletus <smartchibuike2019@gmail.com>
2023-10-25 19:15:41 +0100

    Task nav bar

---
+++ src/components/tasknav.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./task.css";
+
+export const Box = (): JSX.Element => {
+  return (
+    <div className="box">
+      <div className="navigation">
+        <div className="group">
+          <div className="samantha">Projects</div>
+          <div className="text-wrapper">Settings</div>
+          <div className="div">Help</div>
+        </div>
+        <div className="group-2">
+          <div className="samantha-2">TASK</div>
+          <div className="overlap-group-wrapper">
+            <div className="overlap-group">
+              <div className="samantha-3">T</div>
+            </div>
+          </div>
+        </div>
+        <div className="overlap-wrapper">
+          <div className="overlap">
+            <div className="samantha-4">Search everything</div>
+            <img className="img" alt="Group" src="group-2.png" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
