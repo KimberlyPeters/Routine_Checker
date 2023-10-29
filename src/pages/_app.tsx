224a47f7d318dae976124dce0cf160d550c25b88
ConquerorCletus <smartchibuike2019@gmail.com>
2023-10-29 17:49:42 +0100

    dashboard

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
 import "@/styles/globals.css";
-import "@/styles/task.module.css";
 import type { AppProps } from "next/app";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import "@/styles/dashboard.css"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,33 +1,35 @@
 import React from "react";
-import styles from "../styles/task.module.css";
 import logo from "../../public/logo.svg";
 
-const TaskNav = () => {
+
+const dashboard = () => {
   return (
-    <div className={styles.box}>
-      <div className={styles.navigation}>
-        <div className={styles.group}>
-          <div className={styles.samantha}>Projects</div>
-          <div className={styles.textwrapper}>Settings</div>
-          <div className={styles.div}>Help</div>
+    <>
+     <div className="navbox">
+      <div className="navigation">
+        <div className="group">
+          <div className="samantha">Projects</div>
+          <div className="text-wrapper">Settings</div>
+          <div className="div">Help</div>
         </div>
-        <div className={styles.group2}>
-          <div className={styles.samantha2}>TASK</div>
-          <div className={styles.overlapgroupwrapper}>
-            <div className={styles.overlapgroup}>
-              <div className={styles.samantha3}>T</div>
+        <div className="group-2">
+          <div className="samantha-2">TASK</div>
+          <div className="overlap-group-wrapper">
+            <div className="overlap-group">
+              <div className="samantha-3">T</div>
             </div>
           </div>
         </div>
-        <div className={styles.overlapwrapper}>
-          <div className={styles.overlap}>
-            <div className={styles.samantha4}>Search everything</div>
-            <img className={styles.img} alt="Group" src={logo} />
+        <div className="overlap-wrapper">
+          <div className="overlap">
+            <div className="samantha-4">Search everything</div>
+            <img className="img" alt="Group" src="group-2.png" />
           </div>
         </div>
       </div>
     </div>
+    </>
   );
 };
 
-export default TaskNav;
+export default dashboard;
