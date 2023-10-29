3de32438c6e52359b40e426922cbe49e87e1a9eb
ConquerorCletus <smartchibuike2019@gmail.com>
2023-10-29 18:15:21 +0100

    add task in dashboard

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,8 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import "@/styles/dashboard.css"
+import "@/styles/dashboard.css";
+import "@/styles/task.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -7,26 +7,71 @@ const dashboard = () => {
     <>
      <div className="navbox">
       <div className="navigation">
-        <div className="group">
-          <div className="samantha">Projects</div>
-          <div className="text-wrapper">Settings</div>
-          <div className="div">Help</div>
+        <div className="navgroup">
+          <div className="navsamantha">Projects</div>
+          <div className="navtext-wrapper">Settings</div>
+          <div className="navdiv">Help</div>
         </div>
-        <div className="group-2">
-          <div className="samantha-2">TASK</div>
-          <div className="overlap-group-wrapper">
-            <div className="overlap-group">
-              <div className="samantha-3">T</div>
+        <div className="navgroup-2">
+          <div className="navsamantha-2">TASK</div>
+          <div className="navoverlap-group-wrapper">
+            <div className="navoverlap-group">
+              <div className="navsamantha-3">T</div>
             </div>
           </div>
         </div>
-        <div className="overlap-wrapper">
-          <div className="overlap">
-            <div className="samantha-4">Search everything</div>
-            <img className="img" alt="Group" src="group-2.png" />
+        <div className="navoverlap-wrapper">
+          <div className="navoverlap">
+            <div className="navsamantha-4">Search everything</div>
+            <img className="navimg" alt="Group" src="logo.svg" />
           </div>
         </div>
       </div>
+      <div className="create-new-card">
+        <img className="line" alt="Line" src="line-3.svg" />
+        <div className="group">
+          <div className="rectangle-copy">
+            <div className="overlap-group">
+              <div className="homepage-design">+</div>
+            </div>
+          </div>
+          {/* <Tag className="basic-tag" /> */}
+          <div className="box">
+          <div className="overlap-wrapper">
+            <div className="overlap">
+              <div className="rectangle" />
+              <div className="text-wrapper">UI Design</div>
+            </div>
+          </div>
+        </div>
+        <div className="div">
+          <div className="vector-wrapper">
+            <img className="vector" alt="Vector" src="vector.svg" />
+          </div>
+          <img className="img" alt="Rectangle copy" src="rectangle-2-copy-8.png" />
+          <div className="homepage-design-2">Samantha</div>
+          <div className="overlap-group-wrapper">
+            <div className="homepage-design-wrapper">
+              <div className="homepage-design">+</div>
+            </div>
+          </div>
+        </div>
+        <div className="overlap-2">
+          <img className="menu-close-small" alt="Menu close small" src="close-small.svg" />
+          <div className="group-2">
+            <div className="homepage-design-3">Create New Card</div>
+            <div className="div-wrapper">
+              <div className="homepage-design-4">What is the task?</div>
+            </div>
+          </div>
+        </div>
+        <div className="group-3">
+          <div className="overlap-3">
+            <div className="homepage-design-5">Done</div>
+          </div>
+        </div>
+      </div>
+    </div>
     </div>
     </>
   );
