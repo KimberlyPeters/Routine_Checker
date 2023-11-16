99bce3b133ff1021aa6eca5be550b9f7b95bf618
ConquerorCletus <smartchibuike2019@gmail.com>
2023-11-16 16:04:56 +0100

    privateRoute

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,19 @@
 import "@/styles/globals.css";
-// import '../../public/scripts/alpine-script.js'; 
+// import '../../public/scripts/alpine-script.js';
 import type { AppProps } from "next/app";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import '../styles/features.css';
+import "../styles/features.css";
 import "@/styles/dashboard.css";
+import PrivateRoute from "@/components/PrivateRoute";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <ToastContainer />
-      <Component {...pageProps} />
+      <PrivateRoute>
+        <Component {...pageProps} />
+      </PrivateRoute>
     </>
   );
 }
